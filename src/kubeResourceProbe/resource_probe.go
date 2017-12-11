0dbed4717a05ab21727a75788829b3abe254728b
dongyi <surface970>
2017-12-11 15:43:09 +0800

    a

--- src/kubeResourceProbe/resource_probe.go
+++ src/kubeResourceProbe/resource_probe.go
@@ -7,36 +7,54 @@ import (
     "os"
 )
 
-type ResourceProbe struct{
-	confFilePath string
-	client *k8s.Client
-}
+type  (
+	ResourceProbe struct{
+		client *k8s.Client
+	}
+    WatchableResources struct{
+    	configmaps []string
+    	secrets []string
+	}
+)
 
 func Init() {
 	os.Setenv("KUBERNETES_SERVICE_HOST", "kubernetes.default")
 	os.Setenv("KUBERNETES_SERVICE_PORT", "443")
 }
 
-func (*ResourceProbe) InitClient() (*k8s.Client){
+func (*ResourceProbe) initClient() (*k8s.Client){
 	client, err := k8s.NewInClusterClient()
 	if err != nil {
 		glog.Error("Failed to init client!", err)
 	} else {
 		glog.Info("Succeed initing client!")
 	}
-
 	return client
 }
 
-func (pp *ResourceProbe) ListNodes() {
-	pods, _ := pp.InitClient().CoreV1().ListPods(context.Background(), "app-ns")
-	glog.Info(len(pods.Items))
+func (pp *ResourceProbe) WatchResource(resources WatchableResources) {
+	if pp.client == nil {
+		pp.client = pp.initClient()
+	}
+	CoreV1ConfigMapWatcher, err := pp.client.CoreV1().WatchConfigMaps(context.Background(), "app-ns")
+	defer CoreV1ConfigMapWatcher.Close()
+	if err != nil {
+		glog.Error("Watch Configmaps Failed!")
+	} else {
+		glog.Info("Now watching configmaps...")
+		go pp.watch(CoreV1ConfigMapWatcher)
+	}
 }
 
-func (*ResourceProbe) WatchResource() {
-
+func (*ResourceProbe) watch(CoreV1ConfigMapWatcher *k8s.CoreV1ConfigMapWatcher){
+	if event, _, err := CoreV1ConfigMapWatcher.Next(); err != nil {
+		glog.Error("Failed to watch configmaps")
+	} else {
+		if *event.Type == k8s.EventModified {
+			glog.Info("configMap is modified..")
+		}
+	}
 }
-
 
 
 
