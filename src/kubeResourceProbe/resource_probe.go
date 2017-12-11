84f08025a1d3193e34eeae4dcd50045125534d6a
dongyi <surface970>
2017-12-11 14:19:47 +0800

    a

--- src/kubeResourceProbe/resource_probe.go
+++ src/kubeResourceProbe/resource_probe.go
@@ -4,23 +4,27 @@ import (
 	"github.com/ericchiang/k8s"
 	"github.com/golang/glog"
 	"context"
+	"io/ioutil"
+	"fmt"
+	"github.com/ghodss/yaml"
 )
 
 type ResourceProbe struct{
+	confFilePath string
+	client *k8s.Client
 }
 
-func (*ResourceProbe) InitClient() (*k8s.Client){
-	client, err := k8s.NewInClusterClient()
+func (pp *ResourceProbe) initClient() (*k8s.Client){
+	_client, err := k8s.NewInClusterClient()
 	if err != nil {
-		glog.Error("Failed to init client ", err)
-	} else {
-		glog.Info("Succeed initing client!")
+		glog.Error(err)
+		panic("Failed to get client!")
 	}
-	return client
+	return _client
 }
 
 func (pp *ResourceProbe) ListNodes() {
-	nodes, _ := pp.InitClient().CoreV1().ListNodes(context.Background())
+	nodes, _ := pp.initClient().CoreV1().ListNodes(context.Background())
 	for _, node := range nodes.Items {
 		//fmt.Println(node.String())
 		glog.Info("name=%q schedulable=%t memory:%s/%s\n", *node.Metadata.Name, !*node.Spec.Unschedulable,
